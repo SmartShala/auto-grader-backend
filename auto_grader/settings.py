ec70f18ff8e1279c08dc058fce69f55bf336edc4
imon@Mezchip <royimonroy@gmail.com>
2022-04-04 00:05:01 +0530

    updated

--- auto_grader/settings.py
+++ auto_grader/settings.py
@@ -127,6 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 BASE_URL = credentials.get('base_url', '')
 AUTH_USER_MODEL = 'users.User'
 
