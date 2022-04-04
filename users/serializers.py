4b7cd5f21fe4aaa29d203877f17cbe9c4831a1c9
int-code <pubalibasak.cse2020@nsec.ac.in>
2022-04-05 01:20:13 +0530

    Added contact to student api

--- users/serializers.py
+++ users/serializers.py
@@ -2,8 +2,15 @@ from rest_framework import serializers
 from users.models import *
 
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('contact', 'id')
 
 class StudentBodySerializer(serializers.ModelSerializer):
+    contact = serializers.IntegerField(source="user.contact")
     class Meta:
         model = Student
-        fields = ('id','name','student_id','roll_no','academic_year','semester')
+        depth = 1
+        fields = ('contact', 'id','name','roll_no','academic_year','semester')
+
