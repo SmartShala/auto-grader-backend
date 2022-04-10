659edfe20a125fcc7a4a8f5284deb52fd203fdd4
imon.roy <imon.roy@orchids.edu.in>
2022-04-11 03:32:51 +0530

    Added models

--- landing_page/models.py
+++ landing_page/models.py
@@ -12,3 +12,18 @@ class Testimonial(models.Model):
         
     def __str__(self):
         return self.name
+
+
+class QueryData(models.Model):
+    name = models.CharField(max_length=100,null=True,blank=True)
+    email = models.EmailField(unique=True,null=True,blank=True,error_messages={'unique':'This email already exists!'})
+    ph_no = models.BigIntegerField(null=True,blank=True)
+    message = models.TextField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add = True)
+    
+    class Meta:
+        db_table = 'query_data'
+    
+    def __str__(self):
+        return self.name + ' ' + self.email
+    
