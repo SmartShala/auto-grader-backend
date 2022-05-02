3cd1f896cc8ce9c6a646112134615214ffb30c3d
imon.roy <38348296+unownone@users.noreply.github.com>
2022-05-02 22:52:37 +0530

    Fixed csrf trust origin

--- auto_grader/settings.py
+++ auto_grader/settings.py
@@ -184,7 +184,7 @@ REST_FRAMEWORK = {
 
 
 CORS_ALLOWED_ORIGINS = ['https://*','http://*',]
-CSRF_TRUSTED_ORIGINS = ['*.smartshala.live','127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://*.smartshala.live','http://127.0.0.1','http://*.smartshala.live']
 CORS_ALLOW_ALL_ORIGINS = True 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
